176dd00c27978dbc391860a1d0e6e066bd9ea606
Peter Kos <pkos91@icloud.com>
2017-11-26 00:56:30 -0800

    Basic setup for reading battery percentage

--- Save Me From The Light/ViewController.swift
+++ Save Me From The Light/ViewController.swift
@@ -9,10 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	var batteryLevel: Float {
+		return UIDevice.current.batteryLevel
+	}
+	
+	@objc func batteryStateDidChange(_ notification: Notification) {
+		// State change
+		// Reset message when plugged in
+	}
+	
+	@objc func batteryLevelDidChange(_ notification: Notification) {
+		// Level change
+		// Play sound when < 10%, 5%, etc.
+	}
+	
+	// Useful print state function
+	func printState(_ state: Int) -> String {
+		switch UIDevice.current.batteryState {
+		case .charging:  return "Charging!"
+		case .full:		 return "Full!"
+		case .unknown:	 return "Unknown?"
+		case .unplugged: return "Unplugged."
+		}
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+
+		UIDevice.current.isBatteryMonitoringEnabled = true
+		
+		// Observers for level & state change
+		NotificationCenter.default.addObserver(self, selector: #selector(batteryStateDidChange),
+											   name: .UIDeviceBatteryStateDidChange, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(batteryLevelDidChange),
+											   name: .UIDeviceBatteryLevelDidChange, object: nil)
+		
+		print("State: \(printState(UIDevice.current.batteryState.rawValue))")
+		print("Level: \(UIDevice.current.batteryLevel)")
 	}
 
 	override func didReceiveMemoryWarning() {
